7234c73733ae5303aa6c288b8c7904c3ed2d2f23
1m1 <email@1m1.io>
2024-02-09 10:31:44 -0800

    should work, not tested yet

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const configureMiddleware = require('./middleware');
 const configureRoutes = require('./routes');
 const socketio = require('socket.io');
 const gameSocket = require('./socket/index');
+const { Web3 } = require('web3');
 
 // Connect and get reference to mongodb instance
 // let db;
@@ -22,6 +23,28 @@ configureMiddleware(app);
 
 // Set-up Routes
 configureRoutes(app);
+
+// init Web3
+const web3 = new Web3('https://mainnet.infura.io/v3/69ecd65c6b784803bb45b8370caff369');
+
+// new API added here
+// fetching the balance of an Ethereum address
+app.get('/imiapitest', async (req, res) => {
+  // hardcoded for now
+  const contractAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // USDC
+  const accountAddress = '0x6081258689a75d253d87cE902A8de3887239Fe80'; // Robinhood
+  const contractABI = [];
+
+  try {
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      const balance = await contract.methods.balanceOf(accountAddress).call();
+      console.log(`Balance: ${balance}`);
+      res.json({ balance: balance });
+  } catch (error) {
+      console.error(`Error fetching data from smart contract: ${error.message}`);
+      res.status(500).send('Error fetching data from the smart contract');
+  }
+});
 
 // Start server and listen for connections
 const server = app.listen(config.PORT, () => {
